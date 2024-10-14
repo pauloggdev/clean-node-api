d8ff3845778f74bf322f5bcc024b5878a07a7c6d
pauloggdev <pauloggjoao@gmail.com>
2024-10-14 21:52:31 +0100

    ensure LoginRouter returns a custom error if no email or password are provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,10 +1,35 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email || !httpRequest.body.password) {
-      return {
-        statusCode: 400
-      }
+    if (!httpRequest || !httpRequest.body) {
+      return HttpResponse.serverError()
     }
+    const { email, password } = httpRequest.body
+    if (!email) {
+      return HttpResponse.badRequest('email')
+    }
+    if (!password) {
+      return HttpResponse.badRequest('password')
+    }
+  }
+}
+class HttpResponse {
+  static badRequest (paramName) {
+    return {
+      statusCode: 400,
+      body: new MissingParamError(paramName)
+    }
+  }
+
+  static serverError () {
+    return {
+      statusCode: 500
+    }
+  }
+}
+class MissingParamError extends Error {
+  constructor (paramName) {
+    super(`Missing required parameter: ${paramName}`)
+    this.name = 'MissingParamError'
   }
 }
 describe('Login Router', () => {
@@ -17,6 +42,7 @@ describe('Login Router', () => {
     }
     const httResponse = sut.route(httpRequest)
     expect(httResponse.statusCode).toBe(400)
+    expect(httResponse.body).toEqual(new MissingParamError('email'))
   })
   test('Should return 400 if no password is provided', () => {
     const sut = new LoginRouter()
@@ -27,5 +53,17 @@ describe('Login Router', () => {
     }
     const httResponse = sut.route(httpRequest)
     expect(httResponse.statusCode).toBe(400)
+    expect(httResponse.body).toEqual(new MissingParamError('password'))
+  })
+  test('Should return 500 if no httpRequest is provided', () => {
+    const sut = new LoginRouter()
+    const httResponse = sut.route()
+    expect(httResponse.statusCode).toBe(500)
+  })
+  test('Should return 500 if no httpRequest has no body', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {}
+    const httResponse = sut.route(httpRequest)
+    expect(httResponse.statusCode).toBe(500)
   })
 })
